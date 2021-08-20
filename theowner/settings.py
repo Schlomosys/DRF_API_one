924ea303564e711e8ec4b25573805aeaafd0306b
Schallom Awolou <64165396+Schlomosys@users.noreply.github.com>
2021-08-20 20:37:51 +0200

    Update settings.py

--- theowner/settings.py
+++ theowner/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'theowner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-     #   'ENGINE': 'django.db.backends.postgresql',
-     #   'NAME': 'organizations',
-     #   'USER': 'postgres',
-     #   'PASSWORD': 'root',
-     #   'HOST': '127.0.0.1',
-     #   'PORT': '5432',
-    #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'organizations',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
