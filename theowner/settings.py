f782c32c5cfc704b705fb2f0b37e2bb7a2819479
Schallom Awolou <64165396+Schlomosys@users.noreply.github.com>
2021-08-20 23:49:39 +0200

    Update settings.py

--- theowner/settings.py
+++ theowner/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY =os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.environ['DEBUG']
 
-#ALLOWED_HOSTS = [drfschallom.herokuapp.com]
+ALLOWED_HOSTS = ['drfschallom.herokuapp.com']
 
 
 # Application definition
