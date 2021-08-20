0c4f69525ae33670fe951d124c7447a912ebb0ff
Schallom Awolou <64165396+Schlomosys@users.noreply.github.com>
2021-08-20 20:15:52 +0200

    Update settings.py

--- theowner/settings.py
+++ theowner/settings.py
@@ -152,5 +152,3 @@ REST_FRAMEWORK = {
 import dj_database_url 
 db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
 
-import dj_database_url 
-db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
