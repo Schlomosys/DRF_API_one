ee39cae9995833b717ddedd2e46931b9258a385a
Awolou Schallom <schal97.aw@gmail.com>
2021-08-20 20:03:38 +0200

    Add databse for heroku

--- theowner/settings.py
+++ theowner/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'theowner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'organizations',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.postgresql',
+     #   'NAME': 'organizations',
+     #   'USER': 'postgres',
+     #   'PASSWORD': 'root',
+     #   'HOST': '127.0.0.1',
+     #   'PORT': '5432',
+    #}
+#}
 
 
 # Password validation
@@ -149,6 +149,7 @@ REST_FRAMEWORK = {
     
     ),
 }
-
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
 
 
